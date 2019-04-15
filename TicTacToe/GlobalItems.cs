d27ef885bd82f7b71af365257995566ae3937d2c
ashu <ashutoshsinghtomar.ashu@gmail.com>
2019-04-15 13:31:23 +0530

    Global lists

---
+++ TicTacToe/GlobalItems.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using TicTacToe.Model;
+
+
+namespace TicTacToe
+{
+    public class GlobalItems
+    {
+        public static List<Users> OnlineUsers = new List<Users>();
+        public static int TotalPlayersOnline = 0;
+        public static List<GameRequests> Requests = new List<GameRequests>();
+
+        public static List<LiveMatch> OngoingMatches=new List<LiveMatch>(); 
+
+
+    }
+}
