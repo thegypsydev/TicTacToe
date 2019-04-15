f2bebebdd7bb23dc8e874409410cd4c1c94ca77c
ashu <ashutoshsinghtomar.ashu@gmail.com>
2019-04-15 13:29:08 +0530

    Dashboard designed

---
+++ TicTacToe/DashBoard.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DashBoard.aspx.cs" Inherits="TicTacToe.DashBoard" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+  <h2 id ="heading_dashboard" runat="server" ClientIDMode="Static">Online Players</h2>
+    
+    <div>
+        <table id="players_table" runat="server" ClientIDMode="Static">
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Username</th>
+                <th>Games Played</th>
+                <th>Games Won</th>
+                <th>Status</th>
+                <th></th>
+
+            </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+    </div>
+    
+    <div id="confirmBox" title="Challenege The Opponent" style="display: none">
+  <p><span class="ui-icon ui-icon-info" style="float:left; margin:12px 12px 20px 0;"></span>
+      If the User accepts your Request, you both would be connected.</p>
+</div>
+    
+     <div id="acceptRequestBox" title="Accept the Challenge" style="display: none">
+  <p id="challengersDetail" ><span class="ui-icon ui-icon-info" style="float:left; margin:12px 12px 20px 0;"></span>
+     Let's Get It Started.</p>
+</div>
+    
+
+    
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"/>
+  <script src="Scripts/jquery-1.10.2.min.js"></script>
+    <script src="js/dashboard.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+
+</body>
+</html>
---
+++ TicTacToe/DashBoard.aspx.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TicTacToe.Model;
+
+namespace TicTacToe
+{
+    public partial class DashBoard : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        [WebMethod(EnableSession = true)]
+        public static List<Users> GetUsersList()
+        {
+            return GlobalItems.OnlineUsers;
+        }
+
+
+        [WebMethod(EnableSession = true)]
+        public static List<GameRequests> GetGameRequestsList()
+        {
+            return GlobalItems.Requests;
+        }
+
+        [WebMethod(EnableSession = true)]
+        public static List<LiveMatch> GetLiveMatchList()
+        {
+            return GlobalItems.OngoingMatches;
+        }
+
+        [WebMethod(EnableSession = true)]
+        public static void AddGameRequest(string sentto,string sentby, string status,string sender,string receiver)
+        {
+                GlobalItems.Requests.Add(new GameRequests(sentto,sentby,status,sender,receiver));
+            
+            
+            
+        }
+
+
+        [WebMethod(EnableSession = true)]
+        public static void RemoveRequest(string sender, string receiver)
+        {
+                for(var i=0;i<GlobalItems.Requests.Count;i++)
+                {
+                    GameRequests item = GlobalItems.Requests[i];
+                var sentBy = item.SenderName;
+                var sentTo = item.ReceiverName;
+                if (sentTo != null && sentBy!=null)
+                    if (sentTo == receiver && sentBy == sender)
+                    {
+                        GlobalItems.Requests.RemoveAt(i);
+
+                    }
+            }
+
+        }
+
+
+        [WebMethod(EnableSession = true)]
+        public static void ChallengeAccepted(string sender, string receiver)
+        {
+
+            GlobalItems.OngoingMatches.Add(new LiveMatch(sender,receiver));
+
+            int a = GlobalItems.OngoingMatches.Count;
+
+            for (int i = 0; i < a; i++)
+            {
+                LiveMatch alpha = GlobalItems.OngoingMatches[i];
+
+                if (alpha != null)
+                {
+                    var abc = alpha.Sender;
+                }
+            }
+
+            for (var i = 0; i < GlobalItems.Requests.Count; i++)
+            {
+                GameRequests item = GlobalItems.Requests[i];
+                var sentBy = item.SenderName;
+                var sentTo = item.ReceiverName;
+                if (sentTo != null && sentBy != null)
+                    if (sentTo == receiver && sentBy == sender)
+                    {
+                        GlobalItems.Requests.RemoveAt(i);
+
+                    }
+            }
+
+            
+            
+
+
+
+
+        }
+    }
+}
